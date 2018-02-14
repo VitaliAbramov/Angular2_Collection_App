e38558307e68ab6a8322dde81bacba6a1a89528a
vitaliAbramov <vitali.abramov5@gmail.com>
2018-02-14 13:58:33 +0200

    modified market component

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -5,10 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './market.component.html'
 })
 export class MarketComponent implements OnInit {
+  collectibles = [
+    {description: 'learn Linux', type: 'Book'},
+    {description: 'Photo of new york city', type: 'Photo'},
+    {description: 'garbage trash ', type: 'Garbage'},
+    {description: 'piece of paper', type: 'Paper'}
+  ];
 
+  onAddToCollection() {
+    alert('hello')
+  }
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
